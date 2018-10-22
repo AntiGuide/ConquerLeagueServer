d8ee1c4d5d2d7c90b123739bfac62aeea4d8f793
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-10-22 13:49:54 +0200

    Created SimpleTCP example

---
+++ ConquerLeagueServer/ConquerLeagueClient/Client.cs
@@ -0,0 +1,33 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConquerLeagueClient {
+    class Client {
+        private const ushort PORT = 47410;
+
+        static void Main(string[] args) {
+            // Set up client
+            var client = new SimpleTcpClient();
+            try {
+                client.Connect("127.0.0.1", PORT);
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Console.Read();
+                return;
+            }
+            client.Delimiter = 0x13;
+            client.AutoTrimStrings = false;
+
+            var replyMsg = client.WriteLineAndGetReply("Hello world!", TimeSpan.FromSeconds(3));
+            Console.WriteLine("Client sent:     " + "Hello world!");
+            Console.WriteLine("Client recieved: " + replyMsg.MessageString);
+            Console.WriteLine((int)replyMsg.MessageString[replyMsg.MessageString.Length - 1]);
+            client.Disconnect();
+            Console.Read();
+        }
+    }
+}
--- ConquerLeagueServer/ConquerLeagueServer/Server.cs
+++ ConquerLeagueServer/ConquerLeagueServer/Server.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleTCP;
 
 namespace ConquerLeagueServer {
     class Server {
+        private static bool stopServer = false;
+        private const ushort PORT = 47410;
+        private static string tmpMessage;
+
         static void Main(string[] args) {
+            // Set up server
+            var server = new SimpleTcpServer().Start(PORT);
+            server.Delimiter = 0x13;
+            server.AutoTrimStrings = false;
+
+            server.DelimiterDataReceived += (sender, msg) => {
+                msg.ReplyLine("You said: " + msg.MessageString);
+                tmpMessage = msg.MessageString;
+                Console.WriteLine("Server recieved: " + tmpMessage);
+                Console.WriteLine("Server sent:     " + "You said: " + msg.MessageString);
+                stopServer = true;
+            };
+            while (!stopServer) {
+                
+            }
+            server.Stop();
+            Console.Read();
+        }
+
+        public static void StopServer() {
+            stopServer = true;
         }
     }
 }
