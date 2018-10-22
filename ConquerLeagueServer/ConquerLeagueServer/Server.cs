fa47188a6e6bcf063da32e21f3d4c6aa1d9af2ca
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-10-22 10:58:59 +0200

    Visual Studio initialization

---
+++ ConquerLeagueServer/ConquerLeagueServer/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConquerLeagueServer {
+    class Server {
+        static void Main(string[] args) {
+        }
+    }
+}
